e96a40c3e481aeb2e87e792e1c86f078d7b68e94
Gabrieli Ribeiro <gabrielirt@hotmail.com>
2023-05-22 16:56:17 -0300

    Reenviando código com identação correta

--- src/main/java/primeira_aula/Pessoa.java
+++ src/main/java/primeira_aula/Pessoa.java
@@ -3,68 +3,68 @@ package primeira_aula;
 import java.text.DecimalFormat;
 
 public class Pessoa {
-	private String nome;
-	private char sexo;
-	private double peso;
-	private double altura;
-	private double imc;
+    private String nome;
+    private char sexo;
+    private double peso;
+    private double altura;
+    private double imc;
+    private String categoriaImc;
+
+    DecimalFormat df = new DecimalFormat("0.00");
+
+    public Pessoa(String nome, char sexo, double peso, double altura) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setImc() {
+        imc = getPeso() / (getAltura() * getAltura());
+
+        this.imc = imc;
+    }
 	
-	DecimalFormat df = new DecimalFormat("0.00");
-	
-	public Pessoa(String nome, char sexo, double peso, double altura) {
-		this.nome = nome;
-		this.sexo = sexo;
-		this.peso = peso;
-		this.altura = altura;
-	}
+    public void situacaoImc(double imcSituacao) {
+        if (this.sexo == 'F') {
+            if (imc <= 19.1) {
+                categoriaImc = "Abaixo do peso";
+            } else {
+                System.out.println("Normal");
+            }
+        } else if (this.sexo == 'M') {
 
-	public String getNome() {
-		return nome;
-	}
+        }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    }
 
-	public char getSexo() {
-		return sexo;
-	}
+    public void exibeDados() {
+        System.out.println("|----- Cadastro! -----|"
+                + "\n|- Nome: " + this.nome
+                + "\n|- IMC: " + df.format(this.imc)
+                + "\n|- Situação do IMC: "
+        );
 
-	public double getPeso() {
-		return peso;
-	}
-
-	public double getAltura() {
-		return altura;
-	}
-
-	public void setImc() {
-		imc = getPeso()/(getAltura()*getAltura());
-		
-		this.imc = imc;
-	}
-	
-	public void situacaoImc(double imcSituacao) {
-
-		if (this.sexo == 'F') {
-			if (imc >= 19.1) {
-				System.out.println("Abaixo do peso");
-			} else {
-				System.out.println("Normal");
-			}
-		} else if (this.sexo == 'M') {
-
-		}
-		
-	}
-
-	public void exibeDados() {
-		System.out.println("|----- Cadastro! -----|"
-				+ "\n|- Nome: " + this.nome 
-				+ "\n|- IMC: " + df.format(this.imc)
-				+ "\n|- Situação do IMC: " + si
-		);
-		
-	}
+    }
 	
 }
